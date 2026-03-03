0da898e9be5c076bb128bce89e3776b8ea4e7761
eithen504 <eithen504@gmail.com>
2026-03-03 16:11:11 +0530

    added inngest middleware

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,35 @@
-import express, { json } from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express, { json } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connectDB } from './lib/db.js';
+import { serve } from "inngest/express";
+import { inngest, functions } from './lib/inngest.js';
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(cors({
     origin: [process.env.ALLOWED_ORIGIN as string],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
-}))
+}));
 
-app.use(json())
+app.use(json());
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(port, () => {
+            connectDB();
+            console.log(`Example app listening on port ${port}`);
+        });
+    } catch (error) {
+        console.log("Error starting the server", error);
+    }
+}
+
+startServer();
